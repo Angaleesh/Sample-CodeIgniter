210344d9e1cc57bb526b42eba7e9df68eb8f96b6
Angaleesh <93910073+Angaleesh02@users.noreply.github.com>
2023-06-21 16:01:39 +0530

    Update crud.sql

--- crud.sql
+++ crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2023 at 12:03 PM
+-- Generation Time: Jun 21, 2023 at 12:31 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -41,7 +41,35 @@ CREATE TABLE `tbl` (
 INSERT INTO `tbl` (`id`, `first_name`, `last_name`, `address`) VALUES
 (1, 'Angaleesh', 'R', '123,tup'),
 (6, 'boopathy', 'k', '1/2,a_palayam'),
-(7, 'jones', 'Dj', '00,tup');
+(7, 'jones', 'Dj', '00,tup'),
+(8, 'raghul', 'M', '5,tup');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `u_id` int(11) NOT NULL,
+  `username` text NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `u_type` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`u_id`, `username`, `password`, `u_type`) VALUES
+(1, 'admin', 'admin', 1),
+(2, 'user1', 'demo1', 2),
+(3, 'user2', 'user2', 2),
+(4, 'demo', '123', 2),
+(5, 'demo1', 'demo1', 2),
+(7, 'demo3', 'demo3', 2),
+(8, 'demo4', 'demo4', 2),
+(10, 'demo6', 'demo6', 2);
 
 --
 -- Indexes for dumped tables
@@ -54,6 +82,12 @@ ALTER TABLE `tbl`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`u_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,7 +95,13 @@ ALTER TABLE `tbl`
 -- AUTO_INCREMENT for table `tbl`
 --
 ALTER TABLE `tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `u_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
